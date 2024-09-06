14055ec962da674b68fd73aebed851004145426d
shunkat <shunkat@icloud.com>
2024-09-06 09:41:56 +0900

    一応他の人も使えるようにREADMEを追記

--- image-resize-convert-upload.sh
+++ image-resize-convert-upload.sh
@@ -1,8 +1,10 @@
+# このディレクトリが置いてある場所を指定して
 path="/Users/user/Develop/image-resize-convert-uploader/"
 
 cd $path
 ./target/debug/image-resize-convert-uploader
 
-git add .
-git commit -m "画像追加"
-git push origin master
+# githubにpushするならコメントアウト解除して
+# git add .
+# git commit -m "画像追加"
+# git push origin master
